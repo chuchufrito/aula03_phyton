8627f3b7d88723dde85f31916703f604e2dfbcab
chuchufrito <ana.jgoncalves@aluno.senac.rio>
2025-11-25 20:43:46 -0300

    Estrutura de decisão IF/ELSE/ELIF

---
+++ aula04/atividades.py
@@ -0,0 +1,16 @@
+# Atividade aula 04
+# Treinando comandos de desconto
+
+# compra = int(input('Qual o valor a sua compra? '))
+
+# if compra >= 250:
+#     print('Você ganhou 16% de desconto')
+#     desconto = compra - (compra*0.16)
+#     print(f'Sua compra ficou em {desconto}')
+# else:
+#     print('O valor da compra é insuficiente para desconto...')
+
+
+
+      
+
---
+++ aula04/exemplo01.py
@@ -0,0 +1,22 @@
+#Verifica maior idade
+# idade = int(input('Informe sua idade '))
+
+# if idade >= 18:
+#     print('Você é maior de idade')
+# else:
+#     print('Você é menor de idade')
+
+
+# Testando mais condições
+# Teste de pontos
+
+pontos = int(input('Quantos pontos você fez? '))
+
+if pontos >= 100:
+    print('Nível máximo!')
+elif pontos >= 50:
+    print('Nível bom!')
+elif pontos >= 25:
+    print('Pode melhorar!')
+else: 
+    print('Pontuação Baixa...')
